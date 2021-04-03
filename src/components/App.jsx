f46546c115db7d62b601e22908eb2ac603b5c5a7
Josue Carlos Zenteno Yave <61275784+Josue-Zenteno@users.noreply.github.com>
2021-04-03 18:37:14 +0200

    App.jsx v2.0.0

---
+++ src/components/App.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import { Navbar, NavbarBrand, Nav, NavItem, Media, Row, Col, Container, Alert } from 'reactstrap';
+import { GoogleLogin } from 'react-google-login';
+
+import config from '../config.js';
+import SpicyLogo from '../images/spicy_logo_blanco.png';
+
+import NoteList from './notes/NoteList';
+
+//Un estilo, para no tener que tener un css
+var imgStyle = {
+  maxWidth: "85px",
+};
+
+export default function App(props){
+  //[variable, funcionCambiaVariable] = estadoPorDefecto
+  const [loginMessage, setLoginMessage] = useState(null); 
+
+  const responseGoogleSuccess = (googleUser) => {
+    //Obtener usuario de google
+    var profile = googleUser.getBasicProfile(); 
+
+    //Variables de sesion (sesion terminara cuando se haga logOut)
+    sessionStorage.setItem('name', profile.getName()); 
+    sessionStorage.setItem('email', profile.getEmail());
+    sessionStorage.setItem('image', profile.getImageUrl());
+    
+    //Una historia es un array de direcciones entonces redireccionamos a /home
+    props.history.push("/home");
+  }
+
+  const responseGoogleFailure = (response) => {
+    //Un estado puede ser lo que nos de la gana, en este caso un componente Alert
+    setLoginMessage(<Alert color="danger">Inicio de sesión incorrecto. Inténtelo de nuevo</Alert>);
+  }
+
+  return(
+    <Container>
+      <Row>
+        <Col>
+          <Navbar color="dark" light expand="md">
+          <Media style={imgStyle} object src={SpicyLogo} alt="Bird"/><NavbarBrand><h4 className="text-white">Day By Day</h4></NavbarBrand>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                {loginMessage}
+                <GoogleLogin
+                  clientId={config.clientID}
+                  buttonText="Sign in with Google"
+                  theme='dark'
+                  onSuccess={responseGoogleSuccess}
+                  onFailure={responseGoogleFailure}
+                />
+              </NavItem>
+            </Nav>
+          </Navbar>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs= "12">
+          <NoteList/>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
