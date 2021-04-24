4f1e3a5f2067abb6691522e6b8ec392fd9c5a60a
Josue Carlos Zenteno Yave <61275784+Josue-Zenteno@users.noreply.github.com>
2021-04-24 22:28:28 +0200

    HeaderDashboard.jsx v1.1.0

--- src/components/HeaderDashboard.jsx
+++ src/components/HeaderDashboard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Collapse, Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import { Collapse, Navbar, NavbarBrand, Nav, NavItem, NavLink, Badge } from 'reactstrap';
 import { GoogleLogout } from 'react-google-login';
 //añadir el spicy logo
 
@@ -8,21 +8,21 @@ import config from '../config.js';
 export default function HeaderDashboard(props){
 
   return (
-    <Navbar color="dark" light expand="md">
+    <Navbar style={{background:"#762C6B"}} light expand="md">
       <NavbarBrand><span className="text-white"><strong>Day By Day:    </strong> {sessionStorage.getItem('name')}</span></NavbarBrand>
       <Collapse navbar>
         <Nav className="ml-auto" navbar>
-          <NavItem>
-            <NavLink href="#" onClick={() => props.onShow(1)}><span className="text-white">Todos los Notes</span></NavLink>
+          <NavItem style={{background:"#762C6B"}}>
+            <NavLink href="#" onClick={() => props.onShow(1)}><h3><Badge style={{background:"#F69C38"}} className="text-white" pill>Dashboard</Badge></h3></NavLink>
           </NavItem>
-          <NavItem>
-            <NavLink href="#" onClick={() => props.onShow(2)}><span className="text-white">Mis Notes</span></NavLink>
+          <NavItem style={{background:"#762C6B"}}>
+            <NavLink href="#" onClick={() => props.onShow(2)}><h3><Badge style={{background:"#935BF7"}} className="text-white" pill>My Notes</Badge></h3></NavLink>
           </NavItem>
-          <NavItem>
+          <NavItem style={{background:"#762C6B"}}>
             <GoogleLogout
               clientId={config.clientID}
               buttonText="Sign Out"
-              theme='dark'
+              theme='light'
               onLogoutSuccess={props.onLogout}
             />
           </NavItem>
